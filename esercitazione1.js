970ec13f955670bb57b007377be12a702699bc37
alessiogirgenti <115689551+alessiogirgenti@users.noreply.github.com>
2022-11-11 08:25:35 +0100

    Add files via upload

---
+++ esercitazione1.js
@@ -0,0 +1,57 @@
+console.log("TEST 2022-11-03")
+let x = 34;
+console.log(typeof(x))
+
+let myString = "my first string"
+console.log("type of string -> ", typeof(myString))
+console.log("myString -> ", myString)
+console.log("myString -> ", myString)
+
+let myStringDoubleApo= "my \"first string"
+console.log("myStringDoubleApo -> ", myStringDoubleApo)
+
+let myStringBSlash = "my \\ \"first string\""
+console.log("myStringBSlash -> ", myStringBSlash)
+
+
+let myStringNLine ="my \n \"first string\""
+console.log("myStringNLine -> ", myStringNLine)
+
+//myStringLength"my \n\"first string\""
+//console.log("myStringLength -> ", myStrinLength)
+
+ let myString0rig = "my first string"
+let myString0rigSliced = "my first string".slice(3 ,6)
+
+console.log("myString0rig -- myString0rigSliced -> ", myString0rig, myString0rigSliced)
+let myString0rigSlicedTwo = "my first string". slice(3)
+console.log("myString0rigSlicedTwo ->", myString0rigSlicedTwo )
+
+let myString0rigSlicedthree = "my first string". slice(-3)
+console.log("myString0rigSlicedthree -> ", myString0rigSlicedthree)
+
+let myString0rigSubstr = "my first string".substring(2, 4)
+console.log("myString0rigSubstr -> ", myString0rigSubstr)
+
+let myStringRepl = "my first string".replace("first", "second")
+console.log("myStringRepl -> ", myStringRepl)
+
+let myStringReplTwo = "my first first string".replace("first", "second")
+console.log("myStringReplTwo -> ", myStringReplTwo )
+
+let myStringReplThree = "my first first string".replaceAll("fist", "second")
+console.log("StringReplThree -> ", myStringReplThree)
+
+let myStringCatWithPlus ="my first string" + "added string"
+console.log("myStringCatWithPlus -> ", myStringCatWithPlus )
+
+/*letnumTostr = 3
+console.log("numToStrNum, numToStrStr -> ", numToStrNum, numToStrStr.toString())*/
+
+/*let strCharAt = "Test"
+console.log("strCharAt", strCharAt.charAt(2), strCharAt.substr(2, 1))*/
+
+function print() {
+return console.log("Ciao Mondo!!!");
+}
+print()
